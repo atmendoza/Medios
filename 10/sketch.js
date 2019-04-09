e96c2ba4fb06506c90ebfee1a28cfbde68b14cae
atmendoza <47353451+atmendoza@users.noreply.github.com>
2019-04-09 07:30:54 -0500

    Add files via upload

---
+++ 10/sketch.js
@@ -0,0 +1,36 @@
+var datos;
+var cuantos;
+var img;
+
+function preload(){
+
+    datos = loadJSON("astros.json");
+  img = loadImage("atronauta2.png");
+  fnd = loadImage("fondo.jpg");
+}
+
+function setup() {
+  background(220);
+  createCanvas(400, 400);
+  text(datos.number, 50, 50);
+  cuantos = datos.number;
+  
+ for(var i = 0; i < cuantos; i = i + 1){
+   
+   
+   var posX = random(width - (512 / 6));
+   var posY = random(height - (512 / 6));
+   fill(0);
+  //ellipse(posX, posY, 20, 20);
+   image(img, posX, posY, 512 / 6, 512/6);
+   var nombre = datos.people[i].name;
+   fill(0);
+   textSize(9);
+   text(nombre, posX, posY);
+  }
+}
+
+function draw() {
+  
+  
+}
